edad243ec60fae06dbb422956b84a33cde3dba66
jessicaSoares <jessicassoarez@gmail.com>
2021-10-14 02:44:04 -0300

    Cards add

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import CardItem from './CardItem';
+import './Cards.css';
 
 function Cards() {
     return (
@@ -8,13 +9,17 @@ function Cards() {
             <div className="cards__container">
                <div className="cards__wrapper">
                    <ul className="cards__items">
-                        <CardItem />
+                        <CardItem src="images/img-9.jpg"
+                        text = "explore as profundezas da selva amazonica"
+                        label = "Aventura"
+                        path = '/servicos'
+                        />
 
                    </ul>
                 </div> 
             </div>
         </div>
-    )
+    );
 }
 
 export default Cards;
