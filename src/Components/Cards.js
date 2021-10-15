d28d7238ea1a04d3117885c428670d53d56a1913
jessicaSoares <jessicassoarez@gmail.com>
2021-10-14 21:03:22 -0300

    Footer add

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -9,21 +9,33 @@ function Cards() {
             <div className="cards__container">
                <div className="cards__wrapper">
                    <ul className="cards__items">
-                        <CardItem src="images/img-9.jpg"
+                        <CardItem src="images/img-3.jpg"
                         text = "explore as profundezas da selva amazonica"
                         label = "Aventura"
                         path = '/servicos'
                         />
-                        <CardItem src="images/img-2.jpg"
+                        <CardItem src="images/img-4.jpg"
                         text = "explore as profundezas da selva amazonica"
                         label = "Aventura"
                         path = '/servicos'
                         />
-                        <CardItem src="images/img-2.jpg"
+                        <CardItem src="images/img-8.jpg"
                         text = "explore as profundezas da selva amazonica"
                         label = "Aventura"
                         path = '/servicos'
                         />
+                        <CardItem src="images/img-8.jpg"
+                        text = "explore as profundezas da selva amazonica"
+                        label = "Aventura"
+                        path = '/servicos'
+                        />
+                        <CardItem src="images/img-8.jpg"
+                        text = "explore as profundezas da selva amazonica"
+                        label = "Aventura"
+                        path = '/servicos'
+                        />
+
+                        
                         
 
                    </ul>
